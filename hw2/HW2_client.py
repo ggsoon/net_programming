dfac613ba557fe836defdbeaf098cc238f9e715b
dusvlf12ro <dusvlf12ro@gmail.com>
2022-03-22 15:32:22 +0900

    20220322

---
+++ hw2/HW2_client.py
@@ -0,0 +1,17 @@
+from http import server
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+addr = ('localhost', 9000)
+sock.connect(addr)
+msg = sock.recv(1024)
+print(msg.decode())
+
+g = 'Gwon Sooncheol'
+sock.send(g.encode())
+
+b = sock.recv(1024)
+c = int.from_bytes(b, 'big')
+print(c)
+
+sock.close()
