7c21faf2f8c8a8b064c15889607479a6ed72ebfa
ggsoon <88361127+ggsoon@users.noreply.github.com>
2022-05-29 13:46:08 +0900

    20220529

--- 2/requests/requests_4.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-rsp = requests.delete('https://httpbin.org/delete')
-print(rsp.text)
-rsp = requests.head('https://httpbin.org/get')
-print(rsp.headers)
-print(rsp.text)
--- 2/requests/simple_browser.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://localhost:8080'
-rsp = requests.get(url)
-print(rsp.status_code)
-print(rsp.headers)
-print(rsp.text)
