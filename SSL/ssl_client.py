9034eb667c356b1fccad1aa2f2c0a3865f93a1f4
ggsoon <88361127+ggsoon@users.noreply.github.com>
2022-06-22 02:41:04 +0900

    20220622

--- SSL/ssl_client.py
+++ SSL/ssl_client.py
@@ -4,6 +4,7 @@ import socket
 hostname = 'home.sch.ac.kr'
 context = ssl.create_default_context()
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 conn = context.wrap_socket(sock, server_hostname=hostname)
 conn.connect((hostname, 443))
 conn.send(b'HEAD /sch/index.jsp HTTP/1.1\r\nHost: home.sch.ac.kr\r\n\r\n')
--- SSL/ssl_client2.py
+++ SSL/ssl_client2.py
@@ -3,7 +3,8 @@ import socket
 
 hostname = 'home.sch.ac.kr'
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-context.load_default_certs() #서버 인증서를 검사하기 위한 기본 CA 인증서를 로드
+context.load_default_certs() 
+#서버 인증서를 검사하기 위한 기본 CA 인증서를 로드
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 conn = context.wrap_socket(sock, server_hostname=hostname)
 conn.connect((hostname, 443))
